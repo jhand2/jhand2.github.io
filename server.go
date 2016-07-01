a1d6c2ee78aca8da468828389016e15c177e143c
Jordan Hand <jordanhand@comcast.net>
2016-06-30 19:35:11 -0700

    cleaned up server

--- server.go
+++ server.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	//"fmt"
-	//"io/ioutil"
 	"log"
 	"net/http"
-	//"strings"
 )
 
 type Handler struct {
@@ -14,17 +11,7 @@ type Handler struct {
 func (this *Handler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	path := req.URL.Path[1:]
 	log.Println(path)
-
-	//data, err := ioutil.ReadFile(string(path))
-
 	http.ServeFile(w, req, path)
-	//if err == nil {
-	//http.ServeFile(w, req, path)
-	//} else {
-	//w.WriteHeader(404)
-	//w.Write([]byte("Not found yo"))
-	//}
-
 }
 
 func main() {
