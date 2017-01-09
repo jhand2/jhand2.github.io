6a3ba813991fe41b656887d5dab4232261e99cea
Jordan Hand <jordanhand@comcast.net>
2017-01-09 10:31:18 -0800

    removed parse dependency

--- js/services/project-service.js
+++ js/services/project-service.js
@@ -1,15 +1,8 @@
 angular.module('ProjectService', []).service('ProjectService', ['$http', function($http) {
     var data = {};
-    var appId = 'vXlQJ1pQO9QfY11JhZhx5nrihIJAx7myRXSr6Had';
-    var restKey = 'nJaTarpoUTLlZwVPnQmYa6lrKNGbUKnj8II63Pld';
-    var baseUrl = 'https://api.parse.com/1';
 
     data.getProjects = function(callback) {
-        var url = baseUrl + '/classes/Project';
-        $http({method:'GET', url: url, headers: { 
-            'X-Parse-Application-Id':appId, 
-            'X-Parse-REST-API-Key':restKey}
-        })
+        $http({method:'GET', url: 'data/projects.json'})
         .success(function(res) {
             callback(res.results);
         });
